4072be03e0d02929c4733a16f0d23e032e65e076
MuhammadMinhaj2479 <162522203+MuhammadMinhaj2479@users.noreply.github.com>
2024-06-02 00:45:42 +0500

    initial commit

---
+++ assignment 1/app.js
@@ -0,0 +1,12 @@
+// Q1 alert("Hello World")
+
+// Q2 alert('Error! Please enter a valid password')
+
+// Q3 alert("Welcome to js land... \nHappy Coding!")
+
+// Q4 alert("welcome to js land")
+
+// alert("happy coding")
+
+// Q5 console.log("hello.. i can run js through my web browser console")
+
